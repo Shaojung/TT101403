08274a0905927f7bf2452348d9f39465e8373837
Shaojung <shaojung@gmail.com>
2016-10-14 10:20:57 +0800

    click button

--- app/src/main/java/com/test/tt101403/FragmentA.java
+++ app/src/main/java/com/test/tt101403/FragmentA.java
@@ -7,6 +7,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 /**
@@ -64,7 +66,16 @@ public class FragmentA extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+        View v = inflater.inflate(R.layout.fragment_a, container, false);
+        Button btn4 = (Button) v.findViewById(R.id.button4);
+        final TextView tv = (TextView) v.findViewById(R.id.textView);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Hello Hello!!");
+            }
+        });
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
