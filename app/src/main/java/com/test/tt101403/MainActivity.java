722701fa923c779f672a68333e183ceb6ab03386
Shaojung <shaojung@gmail.com>
2016-10-14 10:01:38 +0800

    Initial commit

---
+++ app/src/main/java/com/test/tt101403/MainActivity.java
@@ -0,0 +1,31 @@
+package com.test.tt101403;
+
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.frag_container, new FragmentA());
+        ft.commit();
+    }
+
+    public void click2(View v)
+    {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.frag_container, new FragmentB());
+        ft.commit();
+    }
+}
+
